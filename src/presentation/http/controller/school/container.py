798bb941edf40170756bb77e0a707c01fe8b2735
ítalo Oliveira <elsword.fit@gmail.com>
2026-04-29 21:08:38 -0300

    refactor: remove resíduios de comentários desnecessários

--- src/presentation/http/controller/school/container.py
+++ src/presentation/http/controller/school/container.py
@@ -61,13 +61,13 @@ class Container(containers.DeclarativeContainer):
 container = Container()
 
 class Container(containers.DeclarativeContainer):
-    # 1. Instancia o repositório entregando a coleção (verifique se no Atlas é 'escola' ou 'escolas')
+    
     stats_repository = providers.Factory(
         MongoStatsRepository,
         collection=providers.Callable(mongodb.get_collection, "escolas") 
     )
 
-    # 2. Injeta o repositório no Use Case usando EXATAMENTE o mesmo nome
+    
     get_summary_stats_use_case = providers.Singleton(
         GetSummaryStatsUseCase,
         repository=stats_repository
