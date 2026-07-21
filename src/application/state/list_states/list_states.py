22e639041a507f4030ee935176ea0aa079a21517
ittalllo <elsword.fit@gmail.com>
2026-07-21 19:40:38 -0300

    Feat/endpoint estados nordeste (#77)

--- src/application/state/list_states/list_states.py
+++ src/application/state/list_states/list_states.py
@@ -7,6 +7,10 @@ from src.domain.repository.state_repository import IStateRepository
 class ListStatesUseCase:
     def __init__(self, state_repository: IStateRepository):
         self.state_repository = state_repository
+        self.nordeste_siglas = {"AL", "BA", "CE", "MA", "PB", "PE", "PI", "RN", "SE"}
 
     async def execute(self, q: Optional[str] = None) -> List[State]:
-        return await self.state_repository.list_distinct_states(query=q)
+        all_states = await self.state_repository.list_distinct_states(query=q)
+        return [state for state in all_states if state.sigla.upper() in self.nordeste_siglas]
+
+        
